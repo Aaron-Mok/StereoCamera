f751fac3ff9247855f0579682d4507a63e512743
Aaron-mok <aaronmokty@gmail.com>
2025-07-15 22:45:42 -0700

    Add lens shading correction

--- Camera_ISP_RapsberryPI_HQcam.py
+++ Camera_ISP_RapsberryPI_HQcam.py
@@ -25,20 +25,42 @@ while True:
     print("Digital Gain:", metadata["DigitalGain"])
     print("Exposure Time (µs):", metadata["ExposureTime"])
 
-    cv2.imshow("Raw Image", raw)
+    # cv2.imshow("Raw Image", raw)
 
     # Stride/padding correction
     raw = raw[:, 1::2]
-    raw = raw[:,:img_width_px-1]
+    raw_float = raw[:,:img_width_px-1]
     raw_8bit = raw.astype(np.uint8)
     cv2.imshow("Raw Image", raw_8bit)
 
     # TODO: Add black offset subtraction. Calibration script written. Need Curve fitting and get the black offset for each analogue gain value.
 
+    # lens shading correction
+    flat_field_img = np.load("./flat_field_output/flat_field_average.npy")
+    flat_field_img_blurred = cv2.GaussianBlur(flat_field_img, (11, 11), 0)
+    flat_field_img_normalize = flat_field_img_blurred/flat_field_img_blurred.max()
+    lens_shading_correction = flat_field_img_normalize
+    flat_field_img_normalize_8bit = (flat_field_img_normalize*255).astype(np.uint8)
+    cv2.imshow("Flat Field Image", flat_field_img_normalize_8bit)
+
+    # Define contour levels (e.g., pixel values to outline)
+    levels = [150, 200, 250]  # Adjust as needed for contrast
+
+    # Copy for drawing
+    overlay_img = flat_field_img_normalize_8bit.copy()
+
+    for level in levels:
+        ret, thresh = cv2.threshold(overlay_img, level, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(overlay_img, contours, -1, color=0, thickness=1)  # black contours (0)
     
+    cv2.imshow("Gray Image with Black Contours", overlay_img)
+
+    lens_shade_corrected_float = (raw_float * lens_shading_correction)
+    lens_shade_corrected_float_8bit = (lens_shade_corrected_float).astype(np.uint8)
 
     # Demosaic
-    rgb_image = cv2.cvtColor(raw_8bit, cv2.COLOR_BAYER_BGGR2BGR) # This is BIlinear. There are three options: Bilinear, Edge Aware, and Variable Number of Gradients
+    rgb_image = cv2.cvtColor(lens_shade_corrected_float_8bit, cv2.COLOR_BAYER_BGGR2BGR) # This is BIlinear. There are three options: Bilinear, Edge Aware, and Variable Number of Gradients
 
     # Gamma correction
     gamma = 2.2
@@ -47,7 +69,7 @@ while True:
     gamma_corrected = np.clip(corrected * 255, 0, 255).astype(np.uint8)
 
     # Display
-    cv2.imshow("Gamma Corrected", rgb_image)
+    cv2.imshow("Gamma Corrected", gamma_corrected)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
